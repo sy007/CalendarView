d824757d085731f8194b61c9e4939b30a3efd976
sy007 <547616310@qq.com>
2022-06-01 10:28:35 +0800

    docs: 移除docs文档

--- calendar-view-docs/html/scripts/sourceset_dependencies.js
+++
@@ -1,1 +0,0 @@
-sourceset_dependencies='{":calendar-view:dokkaHtml/androidTestRelease":[],":calendar-view:dokkaHtml/debug":[],":calendar-view:dokkaHtml/main":[],":calendar-view:dokkaHtml/release":[]}'
